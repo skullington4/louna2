87e0382058e1e7818a80d511b0b40ac2a452561a
Kevin <ksully121888@gmail.com>
2023-07-08 12:21:54 -0400

    Added some content to the add item page

--- src/pages/Add/Add.jsx
+++ src/pages/Add/Add.jsx
@@ -5,6 +5,17 @@ export default function Add() {
   
 
     return (
-      <div>This is the Add page</div>
+      <>
+      
+        <h2>This is the Add page</h2>
+
+        <h4>Title: </h4>
+        <input type="text" />
+        <h4>Description: </h4>
+        <input type="text" />
+        <h4>Img holder</h4>
+        
+
+      </>
     );
   }
