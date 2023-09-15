0cc20707912ff3025cf5c75721dfdb62726b9f33
Kevin <ksully121888@gmail.com>
2023-09-15 10:10:38 -0400

    Sidebar added but CSS isn't great now

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -8,22 +8,11 @@ import { useNavigate } from 'react-router-dom'
 
 
 export default function About() {
-
-const [items, setItems] = useState([])
-let navigate = useNavigate();
-
-useEffect(() => {
-  async function getItems() {
-    const result = await axios.get("/api/items")
-    setItems(result.data)
-  }
-  getItems()
-}, [])
   
 
     return (
       <>
-          <h1>Collections</h1>
+          <h1>About page</h1>
           
 
       </>
